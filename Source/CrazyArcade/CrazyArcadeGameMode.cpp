6bec1c1150fcd47e8472d9abcfaa687591bfb770
yo <ksst6855@gmail.com>
2023-03-28 19:36:56 +0900

    0328 머지전

--- Source/CrazyArcade/CrazyArcadeGameMode.cpp
+++ Source/CrazyArcade/CrazyArcadeGameMode.cpp
@@ -23,7 +23,7 @@ void ACrazyArcadeGameMode::BeginPlay()
 	Super::BeginPlay();
 
 	MainCamera = Cast<AMainCamera>(GetWorld()->SpawnActor<ACameraActor>(CameraFactory));
-	MainCamera->SetActorLocationAndRotation(FVector(1500.f, 1750.f, 2500.f), FRotator(-90.f, 0.f, 0.f));
+	MainCamera->SetActorLocationAndRotation(FVector(170.f, 650.f, 5450.f), FRotator(-90.f, 0.f, 0.f));
 
 	auto PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
 	if(PlayerController)
---
+++ Source/CrazyArcade/Public/InGamePlayerWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InGamePlayerWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRAZYARCADE_API UInGamePlayerWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
